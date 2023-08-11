7055471f24fbd3cecece91b8a2913ec4d11b60e9
SeungGu Kang <alex9366@naver.com>
2023-08-12 02:32:55 +0900

    Add SignUpActivity

---
+++ app/src/main/java/com/example/safeguard/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.safeguard.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.safeguard.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
