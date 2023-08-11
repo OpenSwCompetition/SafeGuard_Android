c950497540e83f4a8ce991d4c9cafd9fb441ef7f
SeungGu Kang <alex9366@naver.com>
2023-08-12 02:32:30 +0900

    Add SettingFragment

---
+++ app/src/main/java/com/example/safeguard/ui/setting/SettingFragment.kt
@@ -0,0 +1,8 @@
+package com.example.safeguard.ui.setting
+
+import com.example.safeguard.R
+import com.example.safeguard.databinding.FragmentSettingBinding
+import com.example.safeguard.util.binding.BindingFragment
+
+class SettingFragment : BindingFragment<FragmentSettingBinding>(R.layout.fragment_setting, true) {
+}
