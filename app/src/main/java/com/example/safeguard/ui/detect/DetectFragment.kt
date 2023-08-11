89c108c2d6a47f4e2fb71d7e4ef278dc06a86251
SeungGu Kang <alex9366@naver.com>
2023-08-12 02:33:09 +0900

    Add DetectFragment

---
+++ app/src/main/java/com/example/safeguard/ui/detect/DetectFragment.kt
@@ -0,0 +1,9 @@
+package com.example.safeguard.ui.detect
+
+import com.example.safeguard.R
+import com.example.safeguard.databinding.FragmentDetectBinding
+import com.example.safeguard.util.binding.BindingFragment
+
+class DetectFragment : BindingFragment<FragmentDetectBinding>(R.layout.fragment_detect, true) {
+
+}
