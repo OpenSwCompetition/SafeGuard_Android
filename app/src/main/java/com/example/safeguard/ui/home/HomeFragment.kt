c6d67bc4ff2c439f4c4b5a1c740f74f0a0e694c5
SeungGu Kang <alex9366@naver.com>
2023-08-12 02:32:45 +0900

    Add HomeFragment

---
+++ app/src/main/java/com/example/safeguard/ui/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.safeguard.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.safeguard.R
+import com.example.safeguard.databinding.FragmentHomeBinding
+import com.example.safeguard.util.binding.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home, true) {
+
+}
