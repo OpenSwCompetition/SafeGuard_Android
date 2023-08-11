5d67e5f0c3a11fc40e5af64413920dfa4cd6be9d
SeungGu Kang <alex9366@naver.com>
2023-08-12 02:45:48 +0900

    Update MainActivity.kt : BottomNavigationBar 연결

--- app/src/main/java/com/example/safeguard/ui/MainActivity.kt
+++ app/src/main/java/com/example/safeguard/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.safeguard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.safeguard.R
+import com.example.safeguard.databinding.ActivityMainBinding
+import com.example.safeguard.ui.detect.DetectFragment
+import com.example.safeguard.ui.home.HomeFragment
+import com.example.safeguard.ui.setting.SettingFragment
+import com.example.safeguard.util.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initNavigationBar()
+    }
+
+    private fun initNavigationBar() {
+        binding.bottomNav.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.tab_home -> changeFragment<HomeFragment>()
+                    R.id.tab_camera -> changeFragment<DetectFragment>()
+                    R.id.tab_profile -> changeFragment<SettingFragment>()
+                }
+                true
+            }
+            selectedItemId = R.id.tab_home
+        }
+    }
+
+    private inline fun <reified T: Fragment> changeFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment, T::class.java.newInstance())
+            .commit()
     }
 }
